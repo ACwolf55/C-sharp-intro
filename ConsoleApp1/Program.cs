4507b53a972d140ad83396deff0ae3ae6b1bbac4
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-07-06 23:26:36 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Asta";
+            int numMembers;
+            numMembers = 11;
+
+            char letter = 'A';
+            double price = 55.75;
+            bool magicKnight = true;
+
+            
+
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("    ^");
+            Console.WriteLine("   / | ");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");
+
+            Console.WriteLine("Black Bulls captain: " + name);
+            Console.WriteLine("Black Bulls Squad had "+numMembers+" members");
+            Console.WriteLine("Asta magic knight grade: " + letter);
+            Console.WriteLine("Cost of Black Bulls robe: " + price);
+            Console.WriteLine(magicKnight);
+            Console.WriteLine(name[0]);
+
+        }
+    }
+}
